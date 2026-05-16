60f0d42af1d1aaaf409c07de20870453f7258117
Aiden-Rodriguez <a.woodenrod@gmail.com>
2026-05-15 20:16:49 -0700

    init

---
+++ build/OpenPLC Simulator/src/POUS.h
@@ -0,0 +1,28 @@
+#ifndef __POUS_H
+#define __POUS_H
+
+#include "accessor.h"
+#include "iec_std_lib.h"
+
+// PROGRAM MAIN
+// Data part
+typedef struct {
+  // PROGRAM Interface - IN, OUT, IN_OUT variables
+
+  // PROGRAM private variables - TEMP, private and located variables
+  __DECLARE_VAR(BOOL,GREEN)
+  __DECLARE_VAR(BOOL,YELLOW)
+  __DECLARE_VAR(BOOL,RED)
+  __DECLARE_VAR(BOOL,GREENON)
+  __DECLARE_VAR(BOOL,YELLOWON)
+  __DECLARE_VAR(BOOL,REDON)
+  TON GREENTIMER;
+  TON YELLOWTIMER;
+  TON REDTIMER;
+
+} MAIN;
+
+void MAIN_init__(MAIN *data__, BOOL retain);
+// Code part
+void MAIN_body__(MAIN *data__);
+#endif //__POUS_H
